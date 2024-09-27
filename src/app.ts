787531d89f6faf1784a6ea432d20b3c11f43c2e1
MaNNu017 <manucoccoz@gmail.com>
2024-09-27 18:05:10 -0300

    changes in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,12 @@
 import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
-import { client } from './client/client.entity.js';
 import { clientRouter } from './client/client.routes.js';
-import { Order } from './order/order.entity.js';
 import { orderRouter } from './order/order.routes.js';
-import { Product } from './product/product.entity.js';
 import { productRouter } from './product/product.routes.js';
-import { Category } from './category/category.entity.js';
 import { categoryRouter } from './category/category.routes.js';
 import { orm } from './shared/db/orm.js';
 import { RequestContext } from '@mikro-orm/core';
-import { categoryTypeRouter } from './category/categoryType.routes.js';
+import { productBrandRouter } from './product/productBrand.routes.js';
 
 const app = express();
 app.use(express.json());
@@ -24,7 +20,7 @@ app.use('/api/clients', clientRouter);
 app.use('/api/orders', orderRouter);
 app.use('/api/products', productRouter);
 app.use('/api/categories', categoryRouter);
-app.use('/api/categories/types', categoryTypeRouter);
+app.use('/api/products/types', productBrandRouter);
 
 app.get('/', (req: Request, res: Response) => {
     res.send('HelloWorld!');
